9637c9441da9fd1470725ba009aa09b0f56be79a
Christopher N. KATOYI KABA <me@christopher2k.dev>
2023-08-01 18:13:03 -0400

    chore: switch to solid start

---
+++ apps/web/src/components/Counter.tsx
@@ -0,0 +1,13 @@
+import { createSignal } from "solid-js";
+
+import "./Counter.css";
+
+export default function Counter() {
+  const [count, setCount] = createSignal(0);
+  console.log("count", count());
+  return (
+    <button class="increment" onClick={() => setCount(count() + 1)}>
+      Clicks: {count()}
+    </button>
+  );
+}
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,21 +1,12 @@
+import { fileURLToPath, URL } from 'node:url';
+import solid from 'solid-start/vite';
 import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-// import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-  plugins: [
-    /* 
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-    // devtools(),
-    solidPlugin(),
-  ],
-  server: {
-    port: +process.env.VITE_APP_PORT!,
-  },
-  build: {
-    target: 'modules',
-    outDir: './dist',
+  plugins: [solid()],
+  resolve: {
+    alias: {
+      '@app': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
 });

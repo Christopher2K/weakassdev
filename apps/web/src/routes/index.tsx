b8e6fd24a1541682a57a57490ede6339ee968c9c
Christopher N. KATOYI KABA <me@christopher2k.dev>
2023-08-02 19:24:17 -0400

    test: check if panda is working in production

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,13 +1,38 @@
 import { Title } from 'solid-start';
+import { createSignal } from 'solid-js';
 import Counter from '@app/components/Counter';
 import { EXAMPLE } from '@weakassdev/shared';
+import { css, cva } from '@style/css';
+
+const titleCva = cva({
+  base: {
+    fontSize: '12rem',
+  },
+  variants: {
+    kind: {
+      bigTitle: {
+        color: 'red',
+      },
+      smallTitle: {
+        color: 'blue',
+      },
+    },
+  },
+});
 
 export default function Home() {
+  const [variant, setVariant] = createSignal('bigTitle');
+
+  function changeVariant() {
+    setVariant(variant() === 'bigTitle' ? 'smallTitle' : 'bigTitle');
+  }
+
   return (
     <main>
       <Title>Hello World</Title>
       <Counter />
-      <h1>{EXAMPLE}</h1>
+      {/* @ts-expect-error */}
+      <h1 class={titleCva({ kind: variant() })}>{EXAMPLE}</h1>
       <p>
         Visit{' '}
         <a href="https://start.solidjs.com" target="_blank">
@@ -15,6 +40,7 @@ export default function Home() {
         </a>{' '}
         to learn how to build SolidStart apps.
       </p>
+      <button onClick={changeVariant}>Click me</button>
     </main>
   );
 }

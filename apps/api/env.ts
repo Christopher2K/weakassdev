3352b10de0d1484541a846ab3189e186c3758ed0
Christopher N. KATOYI KABA <me@christopher2k.dev>
2023-08-08 15:30:51 -0400

    chore: database plugin for adonis

--- apps/api/env.ts
+++ apps/api/env.ts
@@ -21,4 +21,5 @@ export default Env.rules({
   APP_NAME: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+  DATABASE_URL: Env.schema.string(),
 });

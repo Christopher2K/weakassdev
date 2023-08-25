86fa2e46cdde705cb3b4e26a4882dbd1845fed33
Christopher N. KATOYI KABA <me@christopher2k.dev>
2023-08-24 21:30:07 -0400

    fix: remove comments

--- apps/web/panda.config.ts
+++ apps/web/panda.config.ts
@@ -15,18 +15,13 @@ const globalCss = defineGlobalStyles({
 });
 
 export default defineConfig({
-  // Whether to use css reset
   preflight: true,
   globalCss,
   jsxFramework: 'solid',
 
-  // Where to look for your css declarations
   include: ['./src/**/*.{js,jsx,ts,tsx}', './pages/**/*.{js,jsx,ts,tsx}'],
-
-  // Files to exclude
   exclude: [],
 
-  // The output directory for your css system
   outdir: 'styled-system',
   presets: [weakAssDevPreset],
 });

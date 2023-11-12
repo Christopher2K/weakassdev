a819fabc24f96c116fb630d1b7a2e9542d708875
Christopher N. KATOYI KABA <me@christopher2k.dev>
2023-11-12 13:59:09 -0500

    chore: global dao test

--- apps/api/app/Manager/GlobalManager.ts
+++ apps/api/app/Manager/GlobalManager.ts
@@ -1,3 +1,5 @@
+import { DateTime } from 'luxon';
+
 import Database from '@ioc:Adonis/Lucid/Database';
 import { DatabaseQueryBuilderContract } from '@ioc:Adonis/Lucid/Database';
 
@@ -18,4 +20,34 @@ export async function getEntitiesCount(...tables: string[]): Promise<EntitiesCou
   );
 
   return results.at(0) ?? {};
+}
+
+/**
+ * Get the object count in every table passed as an argument since the last Monday
+ * @throws if a table does not exists
+ * @returns an object where each key represent a count for a specific table. The key name is the table lowercased + `Count` as a suffix
+ */
+export async function getEntitiesCurrentWeekCount(...tables: string[]): Promise<EntitiesCount> {
+  const firstDayOfThisWeek = DateTime.now()
+    .toUTC()
+    .set({ weekday: 1, hour: 0, minute: 0, second: 0 });
+
+  const results = await tables.reduce<DatabaseQueryBuilderContract<EntitiesCount>>(
+    (query, table) =>
+      query.select(
+        Database.from(table)
+          .count('*')
+          .as(`${table.toLowerCase()}Count`)
+          .where('created_at', '>=', firstDayOfThisWeek.toString()),
+      ),
+    Database.query(),
+  );
+
+  return Object.entries(results.at(0) ?? {}).reduce(
+    (acc, [key, item]) => ({
+      ...acc,
+      [key]: +item,
+    }),
+    {},
+  );
 }
---
+++ apps/api/tests/functional/managers/globalManager.spec.ts
@@ -0,0 +1,61 @@
+import { DateTime } from 'luxon';
+import { test } from '@japa/runner';
+
+import Database from '@ioc:Adonis/Lucid/Database';
+
+import * as GlobalManager from 'App/Manager/GlobalManager';
+import Post from 'App/Models/Post';
+import User from 'App/Models/User';
+import PostFactory from 'Database/factories/PostFactory';
+import UserFactory from 'Database/factories/UserFactory';
+
+test.group('[global manager tests]', (group) => {
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction();
+    return () => Database.rollbackGlobalTransaction();
+  });
+
+  test('getEntitiesCurrentWeekCount() - returns how many posts were created this week', async ({
+    assert,
+  }) => {
+    const now = DateTime.now().toUTC();
+    const lastTuesday = now.set({ weekday: 2 });
+
+    await Post.truncate(true);
+    await PostFactory.with('author')
+      .merge({
+        createdAt: lastTuesday,
+        updatedAt: lastTuesday,
+      })
+      .createMany(20);
+
+    const count = await GlobalManager.getEntitiesCurrentWeekCount('Post');
+
+    assert.equal(count['postCount'], 20);
+  });
+
+  test('getEntitiesCurrentWeekCount() - returns how many posts AND users were created this week', async ({
+    assert,
+  }) => {
+    const now = DateTime.now().toUTC();
+    const lastTuesday = now.set({ weekday: 2 });
+
+    await User.truncate(true); // Delete all posts since cascade
+
+    const users = await UserFactory.merge({
+      createdAt: lastTuesday,
+      updatedAt: lastTuesday,
+    }).createMany(5);
+
+    await PostFactory.merge({
+      authorId: users.at(0)!.id,
+      createdAt: lastTuesday,
+      updatedAt: lastTuesday,
+    }).createMany(20);
+
+    const count = await GlobalManager.getEntitiesCurrentWeekCount('Post', 'User');
+
+    assert.equal(count['postCount'], 20);
+    assert.equal(count['userCount'], 5);
+  });
+});
